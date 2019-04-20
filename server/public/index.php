412552fa96d05770505952a03b67b78189770618
Manish Kumar Pathak <pathakmanish86@gmail.com>
2019-04-21 02:47:54 +0530

    Update index.php
    Add access control

--- server/public/index.php
+++ server/public/index.php
@@ -1,4 +1,7 @@
 <?php
+/*
+* Give access control to angular
+*/
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); 
